a675ec13ecfdf820b83ed74ec4e40918b1a19c61
Ahbab <ahbabshahin7788@gmail.com>
2021-12-08 16:56:30 +0600

    Bootstrap navbar been added to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,38 +1,73 @@
 <html>
 <head>
+<!-- Required meta tags -->
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 <title>Online Food Shop</title>
 <link rel="stylesheet" href="{{asset('styles/stylesheet.css')}}">
 <link rel="stylesheet" href="{{asset('styles/style.css')}}">
 </head>
 <body>
-<div class="menu">
-	<nav>
-        
-		<ul>
-            <li><a href="{{route('index')}}" class="brand">Home</a></li>
-			<li><a href="{{route('menu')}}">Menu</a></li>
-			<li><a href="#">About</a></li>
-			<li><a href="{{route('register')}}">Signup</a></li>
-			@if(Auth::check())
-			<li><a href="{{route('logout')}}">Logout</a>
-			</li>
-			@else
-			<li><a href="{{route('login')}}">Login</a>
-				{{-- <ul>
-					<li><a href="admin.php">Admin</a></li>
-					<li><a href="user.php">User</a></li>
-				</ul> --}}
-			</li>
-			@endif
-		</ul>
-	</nav>
-</div>
-<div id="header">Online Food Shop</div>
-<div id="content">
 
-<img src="{{asset('img/back.jpg')}}" alt="">
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+		  <a class="navbar-brand" href="{{route('index')}}">Online Food Shop</a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+			<ul class="navbar-nav mb-2 mb-lg-0">
+			<li class="nav-item">
+			<a class="nav-link active" aria-current="page" href="{{route('index')}}">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="{{route('menu')}}">Menu</a>
+			    </li>
+			@if(Auth::check())
+			<li class="nav-item">
+			  <a class="nav-link" href="{{route('user_profile')}}">Profile</a>
+			</li>
+	
+			<li class="nav-item">
+			  <a class="nav-link" href="{{route('logout')}}">Logout</a>
+			</li>
+	
+			@else
+			<li class="nav-item">
+				<a class="nav-link" href="{{route('login')}}">Login</a>
+			    </li>
+	
+			    <li class="nav-item">
+				<a class="nav-link" href="{{route('register')}}">Signup</a>
+			    </li>
+			@endif
+	
+		    </ul>
+	
+		  </div>
+		</div>
+	    </nav>
+
+	<div id="content">
+
+		<img  src="{{asset('img/back.jpg')}}" alt="">
+	</div>
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
 
 </body>
 </html>
